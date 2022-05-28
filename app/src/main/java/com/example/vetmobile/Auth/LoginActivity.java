ea0032f6b90cc145310ecd1e987054cebe52a3bd
Slonexx <21moriarty21@mail.ru>
2022-05-29 01:26:30 +0600

    Creating an entrance

--- app/src/main/java/com/example/vetmobile/Auth/LoginActivity.java
+++ app/src/main/java/com/example/vetmobile/Auth/LoginActivity.java
@@ -1,22 +1,32 @@
 package com.example.vetmobile.Auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.vetmobile.DateBase.ApiService;
+import com.example.vetmobile.DateBase.Model.DoctorModel;
+import com.example.vetmobile.DateBase.Model.JSONResponseShow;
 import com.example.vetmobile.DateBase.Model.LoginRequired;
+import com.example.vetmobile.DateBase.Model.TimeModel;
 import com.example.vetmobile.DateBase.Model.UserModel;
 import com.example.vetmobile.DateBase.RetrofitClient;
 import com.example.vetmobile.MainActivity;
 import com.example.vetmobile.R;
+import com.example.vetmobile.SaveDateSharedPreferences;
 import com.google.android.material.textfield.TextInputEditText;
 
 import retrofit2.Call;
@@ -26,9 +36,21 @@ import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button bSignIn;
-    TextInputEditText Email, Password;
-    TextView btnRegist;
+    private SharedPreferences sPref;
+    private SaveDateSharedPreferences sharedPreferences;
+
+    private ConstraintLayout LoginLayout, toComeInUserLayout;
+
+    private Button bSignIn;
+    private TextInputEditText Email, Password;
+    private TextView btnRegist;
+
+    private String Token;
+    private int User_id;
+
+    private ImageView ImageProgile, endProfile;
+    private TextView NameProfile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,37 +58,76 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         setFindViewById();
 
-        bSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        Token = sharedPreferences.getSPrefToken(sPref, Token);
 
-              if(TextUtils.isEmpty(Email.getText().toString()) || TextUtils.isEmpty(Password.getText().toString())){
-                  Toast.makeText(LoginActivity.this,"Email / Пароль не верны!", Toast.LENGTH_LONG).show();
-              }
-              else {
-                  login();
-              }
+        if (Token == ""){
+            toComeInUserLayout.setVisibility(View.GONE);
+            LoginLayout.setVisibility(View.VISIBLE);
+            bSignIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    loginProcces();
+                }
+            });
+        } else {
+            LoginLayout.setVisibility(View.GONE);
+            toComeInUserLayout.setVisibility(View.VISIBLE);
+
+            User_id = sharedPreferences.getSPrefUserId(sPref, User_id);
+            getUser(User_id);
 
 
-            }
-        });
+        }
 
 
 
     }
 
-    private void setFindViewById(){
-        bSignIn = findViewById(R.id.bLogin);
-        Email = findViewById(R.id.etLoginEmail);
-        Password = findViewById(R.id.etLoginPassword);
-        btnRegist = findViewById(R.id.bt_login_registr);
-        btnRegist.setOnClickListener(new View.OnClickListener() {
+    private void getUser(int id)
+    {
+        ApiService apiService = RetrofitClient.getInstance().create(ApiService.class);
+        Call<JSONResponseShow> call = apiService.getUserShow(id);
+        call.enqueue(new Callback<JSONResponseShow>() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+            public void onResponse(Call<JSONResponseShow> call, Response<JSONResponseShow> response) {
+                UserModel model = response.body().getUser();
+
+                Glide.with(LoginActivity.this)
+                        .load(model.getPhoto_id())
+                        .circleCrop()
+                        .error(R.drawable.ic_profile)
+                        .into(ImageProgile);
+
+                NameProfile.setText(model.getName());
+
+                ImageProgile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                Toast.makeText(LoginActivity.this, "Добро пожаловать "+ model.getName(), Toast.LENGTH_LONG).show();
+                            }
+                        },700);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(Call<JSONResponseShow> call, Throwable t) {
             }
         });
+    }
+
+
+    private void loginProcces(){
+        if(TextUtils.isEmpty(Email.getText().toString()) || TextUtils.isEmpty(Password.getText().toString())){
+            Toast.makeText(LoginActivity.this,"Email или Пароль не верны!", Toast.LENGTH_LONG).show();
+        } else {
+            login();
+        }
     }
 
     private void login(){
@@ -78,11 +139,19 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<UserModel> call, Response<UserModel> response) {
                 if (response.isSuccessful()){
-                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+
+                    UserModel model = response.body().getUser();
+                    Token = response.body().getToken();
+                    User_id = model.getId();
+
+                    sharedPreferences.setSPrefToken(sPref, Token);
+                    sharedPreferences.setSPrefUserId(sPref, User_id);
+
                     new Handler().postDelayed(new Runnable() {
                         @Override
                         public void run() {
                             startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            Toast.makeText(LoginActivity.this, "Добро пожаловать " + model.getName(), Toast.LENGTH_LONG).show();
                         }
                     },700);
                 }else {
@@ -97,5 +166,39 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void setFindViewById(){
+        sPref = getSharedPreferences("User", 0);
+        sharedPreferences = new SaveDateSharedPreferences();
+        Token = "";
+        User_id = 0;
+
+        LoginLayout = findViewById(R.id.LoginLayout);
+        toComeInUserLayout = findViewById(R.id.toComeInUserLayout);
+
+        Email = findViewById(R.id.etLoginEmail);
+        Password = findViewById(R.id.etLoginPassword);
+        bSignIn = findViewById(R.id.bLogin);
+        btnRegist = findViewById(R.id.bt_login_registr);
+        btnRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageProgile = findViewById(R.id.ivProfileImageLogin);
+        NameProfile = findViewById(R.id.tvProfileNameLogin);
+        endProfile = findViewById(R.id.iv_btn_login_registr);
+        endProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toComeInUserLayout.setVisibility(View.GONE);
+                LoginLayout.setVisibility(View.VISIBLE);
+                loginProcces();
+            }
+        });
+
+    }
 
 }
