ddd428732c1ee7445b660e00851e33e662f43ee8
Slonexx <21moriarty21@mail.ru>
2022-05-29 01:25:52 +0600

    edit model doctor

--- app/src/main/java/com/example/vetmobile/DateBase/Model/DoctorModel.java
+++ app/src/main/java/com/example/vetmobile/DateBase/Model/DoctorModel.java
@@ -1,5 +1,9 @@
 package com.example.vetmobile.DateBase.Model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class DoctorModel {
 
     private int id;
@@ -7,6 +11,12 @@ public class DoctorModel {
     private String Speciality;
     private String photo_id;
     private String Service_id;
+    @SerializedName("Time")
+    private List<TimeModel> Time;
+
+    public List<TimeModel> getTime() {
+        return Time;
+    }
 
     public int getId() {
         return id;
