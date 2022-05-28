a8cd0151218ededdc8e54b3812f3a41ce8368a44
Slonexx <21moriarty21@mail.ru>
2022-05-29 01:28:55 +0600

    new model

---
+++ app/src/main/java/com/example/vetmobile/DateBase/Model/TimeModel.java
@@ -0,0 +1,25 @@
+package com.example.vetmobile.DateBase.Model;
+
+public class TimeModel {
+
+    private int id;
+    private String Receipt_Date;
+    private String Time;
+    private int Doctor_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getReceipt_Date() {
+        return Receipt_Date;
+    }
+
+    public String getTime() {
+        return Time;
+    }
+
+    public int getDoctor_id() {
+        return Doctor_id;
+    }
+}
